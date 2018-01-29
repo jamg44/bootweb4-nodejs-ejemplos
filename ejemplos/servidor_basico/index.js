6efd006bbd8e8957faddb81a507ab9e3eb065253
Javi Miguel <jamg44@gmail.com>
2018-01-29 22:17:55 +0100

    servidor basico

---
+++ ejemplos/servidor_basico/index.js
@@ -0,0 +1,13 @@
+// cargar librería http
+const http = require('http');
+
+// definir un servidor
+const server = http.createServer(function(request, response) {
+  // en cada petición se ejecuta esto:
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.end('Wake up, Neo');
+})
+
+// arrancar el servidor
+server.listen(1337, '127.0.0.1');
+console.log('Servidor arrancado en http://127.0.0.1:1337');
