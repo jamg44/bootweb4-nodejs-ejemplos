825f022914de06f8050b201f673c37a44225fc42
Javi Miguel <jamg44@gmail.com>
2018-02-07 00:35:32 +0100

    usando una promesa y probando async await

--- nodeapi/routes/apiv1/agentes.js
+++ nodeapi/routes/apiv1/agentes.js
@@ -5,15 +5,19 @@ const router = express.Router();
 
 const Agente = require('../../models/Agente');
 
-router.get('/', (req, res, next) => {
-  Agente.find().exec((err, docs) => {
+router.get('/', async (req, res, next) => {
+  /*Agente.find().exec((err, docs) => {
     if (err) {
       next(err);
       return;
     }
     // si no ha habido error
     res.json({ success: true, result: docs });
-  });
+  });*/
+
+  const docs = await Agente.find().exec();
+  res.json({ success: true, result: docs });
+
 });
 
 router.post('/', (req, res, next) => {
