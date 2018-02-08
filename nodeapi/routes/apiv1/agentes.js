fd696fc492e45f2d4f486df9df2f9b67b8acc059
Javi Miguel <jamg44@gmail.com>
2018-02-08 22:21:14 +0100

    otros métodos del API

--- nodeapi/routes/apiv1/agentes.js
+++ nodeapi/routes/apiv1/agentes.js
@@ -5,7 +5,16 @@ const router = express.Router();
 
 const Agente = require('../../models/Agente');
 
-router.get('/', async (req, res, next) => {
+/**
+ * @api {get} /agentes Request list of agents
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
+router.get('/', async (req, res, next) => { // async convierte el resultado en una promesa
+  /**
+   * Con callback
+   */
   /*Agente.find().exec((err, docs) => {
     if (err) {
       next(err);
@@ -15,11 +24,30 @@ router.get('/', async (req, res, next) => {
     res.json({ success: true, result: docs });
   });*/
 
-  const docs = await Agente.find().exec();
-  res.json({ success: true, result: docs });
+  /**
+   * Con promesa
+   */
+  /*Agente.find().exec().then(docs => {
+    res.json({ success: true, result: docs });
+  }).catch(err => {
+    next(err);
+    return;
+  });*/
 
+  // Con async/await
+  try {
+    const docs = await Agente.find().exec(); // si usamos await, la función donde estoy
+                                             // debe tener async
+    
+    res.json({ success: true, result: docs });  
+  } catch(err) {
+    next(err);
+    return;
+  }  
 });
 
+// POST /
+// Añadir un agente
 router.post('/', (req, res, next) => {
   console.log(req.body);
 
@@ -36,8 +64,39 @@ router.post('/', (req, res, next) => {
     }
     res.json({ success: true, result: agenteGuardado });
   });
+});
 
-  
+// DELETE /
+// Elimina un agente
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+    await Agente.remove({_id: _id}).exec();
+    res.json({ success: true });
+  } catch(err) {
+    next(err);
+    return;
+  }
+});
+
+// PUT /
+// Actualiza un agente
+router.put('/:id', async (req, res, next) => {
+  try {
+    const _id = req.params.id;
+    const data = req.body;
+
+    const agenteActualizado = await Agente.findByIdAndUpdate(_id, data, { 
+      new: true // esto es para obtener la nueva versión del documento
+                // tras actualizarlo
+    });
+    
+    res.json({ success: true, result: agenteActualizado });
+
+  } catch(err) {
+    next(err);
+    return;
+  }
 });
 
 module.exports = router;
