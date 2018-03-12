f908f673ce71b70f72f781fcbae8d10fef6a5bc1
Javi Miguel <jamg44@gmail.com>
2018-03-12 23:41:12 +0100

    cluster

--- ejemplos/ejemplo_modulos/index.js
+++ ejemplos/ejemplo_modulos/index.js
@@ -2,9 +2,16 @@
 
 const calculadora = require('./calculadora');
 
+const paso = 5;
+
 console.log(calculadora.suma(1, 5));
 
 console.log(calculadora.resta(1, 5));
+
+// yo como desarrollador creo que paso debe tener 6
+if (paso === 6) {
+  console.log('loquesea')
+}
 
 calculadora.constante_de_calculo = 3.1416;
 
